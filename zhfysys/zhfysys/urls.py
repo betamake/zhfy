06906eb9d7c005cbf1be951bc9678803623b1fb2
betamake <hukunlun@buaa.edu.cn>
2019-11-06 15:19:42 +0800

    增加二级路由

--- zhfysys/zhfysys/urls.py
+++ zhfysys/zhfysys/urls.py
@@ -14,10 +14,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from user import views
+from django.urls import path,include
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('login/', views.login),
-    path('register/', views.register),
+    path('user/', include('user.urls')),
 ]
